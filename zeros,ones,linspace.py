82edc6b280ae5375eaa7425586506d454c1973d2
Victor Huatuco <59738170+VictorHuatuco@users.noreply.github.com>
2021-03-21 16:02:16 -0500

    Matrices de ceros, unos y entre otros

---
+++ zeros,ones,linspace.py
@@ -0,0 +1,14 @@
+import numpy as np
+a = (2,2)
+c = np.zeros(a,dtype = int)
+print(c)
+
+d = (3,3)
+e = np.ones(d,dtype = int)
+print(e)
+
+f = np.arange(12).reshape(3,4)
+print(f)
+
+g = np.linspace(2,3, num = 10)
+print(g)
